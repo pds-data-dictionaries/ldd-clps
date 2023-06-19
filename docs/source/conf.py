248b1b10b77f374c12f341eccecc2fc6f3d38c54
Jennifer Ward <jgward@wustl.edu>
2023-06-19 14:34:12 -0400

    Revised doc files
    Revised conf.py and index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Example Namespace'
+project = 'CLPS Mission Dictionary'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
